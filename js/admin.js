035aa47954fab6fa44d5c104170bbe3ffc736a6b
brantje <brantje@gmail.com>
2018-04-04 16:18:32 +0200

    Small fix

--- js/admin.js
+++ js/admin.js
@@ -4,14 +4,6 @@ function ocNextnoteUrl(url) {
 }
 
 $(document).ready(function() {
-
-	$.getJSON(ocNextnoteUrl('api/v2.0/settings'), function (data) {
-		console.log(data)
-		$.each(data, function (key, value) {
-			// $('[name="'+ key +'"]').val(value);
-		})
-	});
-
 	$('#nextnote-sharemode').change(function() {
 		var val = $(this).val();
 		$.post(ocNextnoteUrl("api/v2.0/settings"), { field: 'sharemode', value: val }, function (data) {
