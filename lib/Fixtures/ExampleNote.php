da346172dde4c43b12b894b79d84e09ce8672b90
sunjam <sunjam@users.noreply.github.com>
2018-04-13 11:41:53 -0700

    Adding example note
    updated example note

--- lib/Fixtures/ExampleNote.php
+++ lib/Fixtures/ExampleNote.php
@@ -24,6 +24,22 @@
 namespace OCA\NextNote\Fixtures;
 
 class ExampleNote {
-	const TITLE = 'Example note';
-	const NOTE_CONTENT = 'Some example note... If you think this content could be better, send a PR';
+	const TITLE = 'Welcome to NextNote';
+	const NOTE_CONTENT = '<h5 style="text-align: center;">Welcome to <a href="https://github.com/brantje/nextnote" target="_blank" rel="noopener">NextNote</a>!&nbsp; Notes are saved in Rich Text format.</h5>
+<p>This app is considered an <strong><span style="background-color: #00ffff; color: #ff0000;">ALPHA</span></strong> release.&nbsp;<strong>Complete note loss is to be expected</strong> until <a href="https://github.com/brantje/nextnote/issues/50">sharing is fixed</a>,&nbsp;<a href="https://github.com/brantje/nextnote/issues/49">a version 1.0 release is added to the Nextcloud Appstore</a>&nbsp;and basic <a href="https://github.com/brantje/nextnote/issues/96" target="_blank" rel="noopener">note import/export functionality</a> is implemented.</p>
+<h5 style="text-align: center;">Thanks for your interest! &nbsp;Still Want to help us test?</h5>
+<p>&nbsp;</p>
+<ol>
+<li>Join our <img src="../../../apps/nextnote/js/vendor/tinymce/plugins/emoticons/img/smiley-cool.gif" alt="cool" />&nbsp;<span style="background-color: #ffcc99;">Telegram Chat</span>: <a href="https://t.me/nextnote">https://t.me/nextnote</a></li>
+<li>You'll need SSH or terminal access to update NextNote to the latest build.</li>
+<li><code>git pull</code> to update to the latest code -- <a href="https://github.com/brantje/nextnote/commits/master">updates are constant</a>.</li>
+<li>Please report any issues with <a href="https://github.com/brantje/nextnote/issues" target="_blank" rel="noopener">NextNote here</a>.</li>
+<li><a href="https://www.tinymce.com" target="_blank" rel="noopener">TinyMCE</a>, the rich text js library for NextNote, can receive bug reports here. <a href="https://github.com/tinymce/tinymce" target="_blank" rel="noopener">https://github.com/tinymce/tinymce</a></li>
+<li>We appreciate your support!</li>
+</ol>
+<h5><strong>FAQ&rsquo;s</strong></h5>
+<p><em>Where is flat file/Markdown support, folder support, Etc.?</em></p>
+<p style="text-align: left; padding-left: 30px;">Right now the focus is on stability and sharing. &nbsp;These additional features are on the hopeful roadmap.</p>
+<p><em>Are keyboard shortcuts supported?</em></p>
+<p style="padding-left: 30px;">Yes, they are! &nbsp;Just press <span style="font-size: 12pt;"><strong>?</strong></span>&nbsp;in the menu to view them!</p>';
 }
